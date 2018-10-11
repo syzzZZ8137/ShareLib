40715ae1711bc85e810a6d2535de4256c3379e7b
syzzZZ8137 <40220872+syzzZZ8137@users.noreply.github.com>
2018-10-11 10:45:04 +0800

    add unittest file

---
+++ QuantLibPricer/testOptionPricer.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 11 10:40:29 2018
+
+@author: gxjy-003
+"""
+
+import unittest
+
+class testOptionPricer(unittest.TestCase):
+    
+   
+    def testPricingFunc(self):
+       pass
+    
+    def testGreeksFunc(self):
+        pass
+    
+    def testPreWork(self):
+        pass
+    
+    def testNumerical_Greeks(self,option):
+        pass
+    
+ 
+    
+    
+if __name__ == '__main__':
+    unittest.main()
---
+++ QuantLibPricer/testOptionProduct.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 11 10:40:07 2018
+
+@author: gxjy-003
+"""
+
