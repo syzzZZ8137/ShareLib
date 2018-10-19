5706366b1fe354415c6820c7b836fc0139d293df
syzzZZ8137 <40220872+syzzZZ8137@users.noreply.github.com>
2018-10-19 12:52:19 +0800

    update payoff simulator
    show pricing of each month

--- QuantLibPricer/PayoffSimulator.py
+++ QuantLibPricer/PayoffSimulator.py
@@ -48,30 +48,37 @@ def get_PV_payoffs(paths, interest_rate, strike_floor, strike_premium):
     '''
     返回所有路径的payoff: numpy.ndarray
     '''
-    PV_payoffs = np.zeros((len(paths), 1))
+    PV_payoffs = np.zeros((len(paths),1)) #np.zeros((len(paths), 1))
+    arr_month_payoffs = np.zeros((len(paths), 7))
     for i in range(len(paths)):
-        PV_payoff_i = get_PV_payoff_by_path(paths[i], interest_rate, strike_floor, strike_premium)
+        PV_payoff_i, arr_month_payoff_i = get_PV_payoff_by_path(paths[i], interest_rate, strike_floor, strike_premium)
         PV_payoffs[i] = PV_payoff_i
-    return PV_payoffs
+        arr_month_payoffs[i] = arr_month_payoff_i
+    return PV_payoffs, arr_month_payoffs
     
     
 def get_PV_payoff_by_path(path, interest_rate, strike_floor, strike_premium):
-    '''返回给定的一条7个月的价格路径的payoff折现后的价格'''  
+    '''返回给定的一条7个月的价格路径的payoff折现后的价格
+    以及各个（单月）期权的payout array(未折现)
+    '''  
     #将长为7个月的path拆分成7条1个月的路径
     interval_paths = []
-    arr_payoff = np.zeros((7, 1))
+    arr_month_payoff = np.zeros((7)) #np.zeros((7, 1))
+    arr_PV_month_payoff = np.zeros((7))
     PV_payoff = 0
          
     for j in range(1,8):
         path_month_j = path[1+30*(j-1):31+30*(j-1)]
         interval_paths.append(path[1+30*(j-1):31+30*(j-1)])
         payoff_j = get_payoff_month_j(path_month_j, strike_floor, strike_premium)
-        arr_payoff[j-1] = payoff_j
+        arr_month_payoff[j-1] = payoff_j
         # discount the payoff to present
         time_to_maturity_j = j/12.0
-        PV_payoff += payoff_j * np.exp(-interest_rate * time_to_maturity_j)
-    return PV_payoff
+        PV_month_payoff_j = payoff_j * np.exp(-interest_rate * time_to_maturity_j)
+        arr_PV_month_payoff[j-1] = PV_month_payoff_j
+        PV_payoff += PV_month_payoff_j
 
+    return PV_payoff, arr_month_payoff
 
 
 def get_payoff_month_j(path_month_j, strike_floor, strike_premium):
@@ -103,7 +110,7 @@ def getPathAvg(path):
     return np.mean(path)    
     
         
-def getStrike(KK, strike_floor=13000, strike_premium=1000):
+def getStrike(KK, strike_floor, strike_premium):
     '''
     strike = max(KK, strike_floor) + strike_premium
     '''
@@ -117,14 +124,55 @@ def getPriceAndStd(PV_payoffs):
     se = np.sqrt((np.sum(PV_payoffs**2)-Nsample*V**2)/Nsample/(Nsample-1))
     return V, se
 
+#arr_PV_month_payoffs  #array, Nsample * 7
+def getPriceDetails(matrix_month_payoffs):
+    '''
+    返回7个月每个月的（折现）价格
+    '''
+    expiry_in_months = matrix_month_payoffs.shape[1]
+    arr_price = np.zeros((7))
+    for j in range(expiry_in_months):
+        month_j_payoffs = np.array(matrix_month_payoffs[:,j])  # Nsample * 1 matrix
+        V_j, se_j = getPriceAndStd(month_j_payoffs)
+        arr_price[j] = V_j
+    return arr_price
+        
+
+def pricefunc(strike_floor, volatility, underlying_price):
+    '''
+    固定underlying_price = 12500
+    调整 strike_floor和volatility 
+    '''
+    
+    print('underlying_price:', underlying_price)
+    print('strike_floor:', strike_floor)
+    
+    ovo = OptionProduct.VanillaOption(underlying_price,strike_price,valuation_date,expiry_date,volatility,interest_rate,dividend_rate,option_type,exercise_type)
+    pricer = OptionPricer.Vanilla_BSM(ovo)
+    option, process = pricer()
+
+    payoffSimulator = PayoffSimulator(process, length, Nsample, Nstep)
+    #生成paths, 计算各路径payoff
+    time_list, paths = payoffSimulator.simulate_paths_for_process(process, length, Nsample, Nstep)
+    
+    PV_payoffs, arr_month_payoffs = get_PV_payoffs(paths, interest_rate, strike_floor, strike_premium)
+    # (Nsample, expiry_in_months)2darray  -> (Nsample, expiry_in_months)矩阵 
+    matrix_month_payoffs = np.asmatrix(arr_month_payoffs)
+    price, std_error = getPriceAndStd(PV_payoffs)
+    arr_price = getPriceDetails(matrix_month_payoffs)
+    
+    print('volatility:', volatility)
+    print('蒙特卡洛模拟定价', price)
+    print('定价标准差', std_error)  
+    print('每月payout均值：', arr_price)
+
 
 if __name__ == '__main__':
      
      # ---------- testing pricing 分段看跌，欧式----------
     
-    t_start = time.time()
-    underlying_price = 13000 #S0
-#    underlying_price = 12000
+    underlying_price = 12500 #S0, 固定
+    strike_premium = 1000
     
     strike_price = underlying_price #用于定义Vanilla平值期权的BSM过程
     valuation_date = dt.datetime.strptime('20181015', '%Y%m%d')
@@ -132,118 +180,48 @@ if __name__ == '__main__':
     expiry_in_months = 7
     expiry_in_days = 7*30
     expiry_date = valuation_date + dt.timedelta(days=expiry_in_days)  # 7个月
-    volatility = 0.25 
-#    volatility = 0.3 
-#    volatility = 0.35 
-#    volatility = 0.4
-    
+
     interest_rate = 0.0
     dividend_rate = 0.0
     option_type = 'put'
     exercise_type = 'E'
-
-    # ---------- MC模拟价格路径，自定义payoff function----------
-    # set option and process
-    #ovo = VanillaOption(13000, 13000, valuation_date, expiry_date, 0.25, 0, 0, 'put','E')
-    ovo = OptionProduct.VanillaOption(underlying_price,strike_price,valuation_date,expiry_date,volatility,interest_rate,dividend_rate,option_type,exercise_type)
-    pricer = OptionPricer.Vanilla_BSM(ovo)
-    option, process = pricer()
     
     Nsample = num_paths = 500000;  
     Nstep = timestep = expiry_in_days #1000;  
     length = (expiry_date - valuation_date).days / 365  #in years
     expiry_in_years = expiry_in_days/365
+    
 
-    payoffSimulator = PayoffSimulator(process, length, Nsample, Nstep)
-    #生成paths, 计算各路径payoff
-    time_list, paths = payoffSimulator.simulate_paths_for_process(process, length, Nsample, Nstep)
-    
-    strike_floor = underlying_price
-    strike_premium = 1000
-    
-    PV_payoffs = get_PV_payoffs(paths, interest_rate, strike_floor, strike_premium)
-    price, std_error = getPriceAndStd(PV_payoffs)
-    
-    print('volatility:', volatility)
-    print('strike_floor:', strike_floor)
-    print('每月回看看跌欧式期权蒙特卡洛模拟定价', price)
-    print('定价标准差', std_error)
-    
-    
-    volatility = 0.3 
-    ovo = OptionProduct.VanillaOption(underlying_price,strike_price,valuation_date,expiry_date,volatility,interest_rate,dividend_rate,option_type,exercise_type)
-    pricer = OptionPricer.Vanilla_BSM(ovo)
-    option, process = pricer()
-    
-    Nsample = num_paths = 500000;  
-    Nstep = timestep = expiry_in_days #1000;  
-    length = (expiry_date - valuation_date).days / 365  #in years
-    expiry_in_years = expiry_in_days/365
-
-    payoffSimulator = PayoffSimulator(process, length, Nsample, Nstep)
-    #生成paths, 计算各路径payoff
-    time_list, paths = payoffSimulator.simulate_paths_for_process(process, length, Nsample, Nstep)
-    
-    strike_floor = underlying_price
-    strike_premium = 1000
-    
-    PV_payoffs = get_PV_payoffs(paths, interest_rate, strike_floor, strike_premium)
-    price, std_error = getPriceAndStd(PV_payoffs)
-    
-    print('volatility:', volatility)
-    print('strike_floor:', strike_floor)
-    print('每月回看看跌欧式期权蒙特卡洛模拟定价', price)
-    print('定价标准差', std_error)    
+#    volatility = 0.2
+#    volatility = 0.25 
+#    volatility = 0.3 
+#    volatility = 0.35     
+#    strike_floor = 13000
+#    strike_floor = 12500
+#    strike_floor = 12000
+        
+#    pricefunc(strike_floor=13000, volatility=0.25, underlying_price=13000)
+    #蒙特卡洛模拟定价 9015.496776983266
+#定价标准差 4.881300309400135
+#每月payout均值： [1064.83010783 1189.12730746 1259.82227881 1314.0763557  1358.24033726
+# 1397.19215285 1432.20823708]
     
     
-    volatility = 0.35
-    ovo = OptionProduct.VanillaOption(underlying_price,strike_price,valuation_date,expiry_date,volatility,interest_rate,dividend_rate,option_type,exercise_type)
-    pricer = OptionPricer.Vanilla_BSM(ovo)
-    option, process = pricer()
-    
-    Nsample = num_paths = 500000;  
-    Nstep = timestep = expiry_in_days #1000;  
-    length = (expiry_date - valuation_date).days / 365  #in years
-    expiry_in_years = expiry_in_days/365
-
-    payoffSimulator = PayoffSimulator(process, length, Nsample, Nstep)
-    #生成paths, 计算各路径payoff
-    time_list, paths = payoffSimulator.simulate_paths_for_process(process, length, Nsample, Nstep)
-    
-    strike_floor = underlying_price
-    strike_premium = 1000
-    
-    PV_payoffs = get_PV_payoffs(paths, interest_rate, strike_floor, strike_premium)
-    price, std_error = getPriceAndStd(PV_payoffs)
-    
-    print('volatility:', volatility)
-    print('strike_floor:', strike_floor)
-    print('每月回看看跌欧式期权蒙特卡洛模拟定价', price)
-    print('定价标准差', std_error)      
+    pricefunc(13000, 0.2, 12500)
+    pricefunc(13000, 0.25, 12500)
+    pricefunc(13000, 0.3, 12500)
+    pricefunc(13000, 0.35, 12500)
     
     
-    volatility = 0.4
-    ovo = OptionProduct.VanillaOption(underlying_price,strike_price,valuation_date,expiry_date,volatility,interest_rate,dividend_rate,option_type,exercise_type)
-    pricer = OptionPricer.Vanilla_BSM(ovo)
-    option, process = pricer()
     
-    Nsample = num_paths = 500000;  
-    Nstep = timestep = expiry_in_days #1000;  
-    length = (expiry_date - valuation_date).days / 365  #in years
-    expiry_in_years = expiry_in_days/365
-
-    payoffSimulator = PayoffSimulator(process, length, Nsample, Nstep)
-    #生成paths, 计算各路径payoff
-    time_list, paths = payoffSimulator.simulate_paths_for_process(process, length, Nsample, Nstep)
     
-    strike_floor = underlying_price
-    strike_premium = 1000
     
-    PV_payoffs = get_PV_payoffs(paths, interest_rate, strike_floor, strike_premium)
-    price, std_error = getPriceAndStd(PV_payoffs)
     
-    print('volatility:', volatility)
-    print('strike_floor:', strike_floor)
-    print('每月回看看跌欧式期权蒙特卡洛模拟定价', price)
-    print('定价标准差', std_error)       
-   
+    
+    
+    
+    
+    
+    
+    
+    
